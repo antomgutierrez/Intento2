d966d60aaed4bbc4c3a86d5e13e84b00350280df
Mateone <54219667+Mateone@users.noreply.github.com>
2019-08-20 20:11:14 -0300

    cambio de master a matero2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace intento2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nicolás DellAcqua");
+            Console.WriteLine("Mateo Negrin");
+        }
+    }
+}
